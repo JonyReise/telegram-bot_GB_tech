6a607c000705023488ffe3c4738229492950ddf9
JonyReise <jonyreise@gmail.com>
2026-04-28 21:37:59 +0300

    Update reply.py

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -3,7 +3,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 def main_reply_keyboard() -> ReplyKeyboardMarkup:
     return ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="📋 Меню"), KeyboardButton(text="ℹ️ О боте")],
+            [KeyboardButton(text="📋 Меню"), KeyboardButton(text="🔁 Змінити магазин")],
             [KeyboardButton(text="⚙️ Настройки"), KeyboardButton(text="📞 Контакты")],
         ],
         resize_keyboard=True,
